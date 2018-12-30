83dbba61b56110716b54dd41bed83e6c458b973e
angel0514 <46083768+angel0514@users.noreply.github.com>
2018-12-30 10:27:22 +0800

    Update base_action.py

--- base/base_action.py
+++ base/base_action.py
@@ -1,5 +1,5 @@
 import time
-
+import allure
 from selenium.webdriver.common.by import By
 
 
@@ -55,5 +55,5 @@ class BaseAction:
         png_name = "./screen/{}.png".format(int(time.time()))
         self.driver.get_screenshot_as_file(png_name)
 
-        # with open("abc.png", "rb") as f:
-        # allure.attach("截图名字", f.read(), allure.attach_type.PNG)
+        with open(png_name, "rb") as f:
+            allure.attach("截图名字", f.read(), allure.attach_type.PNG)
